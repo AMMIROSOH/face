d2e1cb359ca52a9531258d5795b920c069bf17a1
AMMIROSOH <52034721+AMMIROSOH@users.noreply.github.com>
2025-11-16 12:42:23 +0330

    feat: create_engine code for tensorrt

--- src/process/capture.py
+++ src/process/capture.py
@@ -1,6 +1,7 @@
 from multiprocessing import Queue, shared_memory
 import numpy as np
 import cv2 as cv
+import time
 from inference import FRAME_SHAPE
 from constants import GLOBAL_MESSAGE_LENGTH, VIDEO_SOURCE, FRAME_SHAPE_GUI
 
@@ -19,17 +20,28 @@ def capture(shms: tuple[str, ...], q_out: Queue):
         print(f"ERROR: Cannot find camera or movie")
         global_message[0] = 0
         return
+    
+    desired_fps = 60 
+    frame_time = 1 / desired_fps 
 
     try:
         while global_message[0]:
+            start_time = time.time()
             ret, frame_temp = cap.read()
             if(not ret):
                global_message[0] = 0
+               q_out.put(0, False)
+               cap.release()
                break
             cv.resize(frame_temp, (FRAME_SHAPE[1], FRAME_SHAPE[0]), frame)
             cv.resize(frame_temp, (FRAME_SHAPE_GUI[1], FRAME_SHAPE_GUI[0]), gui_frame_in)
-            
             q_out.put(0, False)
+
+            # limiting max fps
+            elapsed_time = time.time() - start_time
+            time_to_wait = frame_time - elapsed_time
+            if time_to_wait > 0:
+                time.sleep(time_to_wait)
     except Exception as e:
         print(e)
         global_message[0] = 0
