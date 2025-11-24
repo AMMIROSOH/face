d89e7216c54ddc2cf3de3afab8584ef460c52171
AMMIROSOH <a.sohrabi1380@gmail.com>
2025-11-24 16:46:07 +0100

    Update arcface.py

--- src/utils/arcface.py
+++ src/utils/arcface.py
@@ -1,12 +1,13 @@
 import numpy as np
 from cv2 import estimateAffinePartial2D, LMEDS
+
 IMG_SIZE = 112 
 LANDS_TEMPLATE = np.array([
-    [30.2946, 51.6963],  # left eye
-    [65.5318, 51.5014],  # right eye
-    [48.0252, 71.7366],  # nose
-    [33.5493, 92.3655],  # left mouth
-    [62.7299, 92.2041],  # right mouth
+    [38.2946, 51.6963],
+    [73.5318, 51.5014],
+    [56.0252, 71.7366],
+    [41.5493, 92.3655],
+    [70.7299, 92.2041]
 ], dtype=np.float32)
 
 def estimate_norm(landmarks, template=LANDS_TEMPLATE):
